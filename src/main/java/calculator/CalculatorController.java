aa34fa47b7b080b8c6e67f579c19785b72fc59e7
ahmedelahwany <61867082+ahmedelahwany@users.noreply.github.com>
2020-05-19 08:21:23 +0200

    Add files via upload

---
+++ src/main/java/calculator/CalculatorController.java
@@ -0,0 +1,85 @@
+package calculator;
+
+import calculator.model.Calculator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML
+    private TextField display;
+
+    private Calculator calculator;
+    private boolean startNumber = true;
+    private double number1;
+    private String operator = "";
+
+    @FXML
+    private void initialize() {
+        calculator = new Calculator();
+    }
+
+    @FXML
+    public void processDigit(ActionEvent event) {
+        String digitPressed = ((Button) event.getSource()).getText();
+        // disallowing mutli dots in single number
+        if(display.getText().contains(".") && digitPressed.equals(".") ) return;
+        System.out.println(digitPressed);
+        if (startNumber || display.getText().equals("0")) {
+            display.setText(digitPressed);
+        } else {
+            display.setText(display.getText() + digitPressed);
+        }
+        startNumber = false;
+    }
+
+    @FXML
+    public void processOperator(ActionEvent event) {
+        String operatorPressed = ((Button) event.getSource()).getText();
+        System.out.println(operatorPressed);
+        if (operatorPressed.equals("=")) {
+           if (operator.isEmpty()) {
+               return;
+           }
+           double number2 = Double.parseDouble(display.getText());
+           double result = calculator.calculate(number1, number2, operator);
+
+               display.setText(String.valueOf(result));
+
+           operator = "";
+        } else {
+            if (! operator.isEmpty()) {
+                return;
+            }
+            number1 = Double.parseDouble(display.getText());
+            operator = operatorPressed;
+            startNumber = true;
+        }
+    }
+
+    @FXML
+    public void ProcessAC(ActionEvent event) {
+        number1 = 0;
+        operator = "";
+        display.setText("0");
+    }
+
+    @FXML
+    public void ProcessSignAlt(ActionEvent event) {
+
+        String CurrentNum = display.getText();
+        if (CurrentNum.length() == 0) return;
+        // To check whether a number has been allocated to number1 variable  to change its value to negative ,in addition number2 has not been recorded
+        if(startNumber){
+            String number =display.getText();
+            number1 = Double.parseDouble(number)* (-1);
+        }
+        // changing signs based on text
+        CurrentNum=CurrentNum.startsWith("-")?CurrentNum.substring(1): "-" + CurrentNum;
+        display.setText(CurrentNum);
+    }
+
+
+}
